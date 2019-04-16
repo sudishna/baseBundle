b454bad7f7eae54aa3936a43ee7e27c1a60d1fff
sudishna <sudishna.sthapit@jeresiliemoncontrat.com>
2019-04-16 11:34:52 +0200

    Initial BaseBundle Commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Clicissimo\BaseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BaseBundle:Default:index.html.twig');
+    }
+}
